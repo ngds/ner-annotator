34aaccc08d182f7de51a29b35d3ac41c46e737ef
Amanda Bertsch <abertsch72@gmail.com>
2020-11-16 18:21:34 -0700

    buggy

--- annotate.py
+++ annotate.py
@@ -1,7 +1,7 @@
 import os
 import sys
 import pickle
-from colorama import Fore, Style
+from colorama import Fore, Style, init
 from string import punctuation
 
 
@@ -12,10 +12,11 @@ valid_inputs = {"0": "PERSON", "1": "NORP", "2": "LOC", "3": "FAC",
 stanford_core_tags = {"PERSON": "PERSON", "NORP": "O", "LOC": "LOCATION", "FAC": "O",
                        "ORG": "O", "GPE": "LOCATION", "EVENT": "O", "QUANTITY": "O"}
 
-stanford_ann = ""
+stanford_ann = []
 spacy_ann = []
 fileout = ""
 pos = 0
+back = 0
 
 def print_tags():
     print("TAG OPTIONS: (press enter to leave untagged, b to go back)")
@@ -26,12 +27,13 @@ def print_tags():
 
 
 def annotate(fp):
+    init()
     file = fp.read()
     words = file.split()
     for i, word in enumerate(words):
         get_tag(i, word, words)
 
-    fps["stanfordnlp-out"].write(stanford_ann)
+    fps["stanfordnlp-out"].writelines(stanford_ann)
     pickle.dump(spacy_ann, fps["spacy-out"])
     fps["rawtext-out"].write(fileout)
 
@@ -41,6 +43,7 @@ def annotate(fp):
 
 
 def get_tag(i, word, words):
+    global back, pos
     print_tags()
     for j in range(i - 3, i):
         if j >= 0:
@@ -54,7 +57,9 @@ def get_tag(i, word, words):
 
     tag = input("\n\tTAG? ")
     if tag == "b" and i > 0:
+        back += 1
         curr = i-1
+        pos -= len(words[curr]) + 1
         while (curr <= i):
             curr = get_tag(curr, words[curr], words) + 1
     elif tag == "" or tag in valid_inputs:
@@ -63,6 +68,8 @@ def get_tag(i, word, words):
         print(f"\n{Fore.RED}Sorry, not sure what that meant. Try again.{Style.RESET_ALL}")
         get_tag(i, word, words)
 
+    if(back > 0):
+        back -= 1
     print()
     return i
 
@@ -74,20 +81,44 @@ def add_spacy_ann(word, tag):
     else:
         spacy_ann.append((pos + 1, pos + 1 + len(word), valid_inputs[tag]))
 
+def write_back_annotation(word, tag):
+    global fileout, stanford_ann, spacy_ann, pos, back
+    if tag == "":
+        stanford_ann[-back] = word + "\t" + "O" + "\n"
+    else:
+        stanford_ann[-back] = word + "\t" + stanford_core_tags[valid_inputs[tag]] + "\n"
+        word = word.strip(punctuation)
+        replaced = False
+        for i in range(back):
+            print(pos)
+            print(spacy_ann[-i][0])
+            if(len(spacy_ann) != 0 and spacy_ann[-i][0] == pos + 1):
+                print("doing something weird")
+                spacy_ann.pop(-i)
+                spacy_ann.insert(-i, ((pos + 1, pos + 1 + len(word), valid_inputs[tag])))
+                replaced = True
+        if not replaced:
+            spacy_ann.append((pos + 1, pos + 1 + len(word), valid_inputs[tag]))
+    pos = pos + 1 + len(word)
+    print("pos is currently " + str(pos))
+
 
 def write_annotation(word, tag):
+    if(back != 0):
+        return write_back_annotation(word, tag)
+
     global fileout, stanford_ann, spacy_ann, pos
 
     if tag == "":
         fileout += " " + word
-        stanford_ann += word + "\t" + "O" + "\n"
+        stanford_ann.append(word + "\t" + "O" + "\n")
     else:
         fileout += " " + word
-        stanford_ann += word + "\t" + stanford_core_tags[valid_inputs[tag]] + "\n"
+        stanford_ann.append(word + "\t" + stanford_core_tags[valid_inputs[tag]] + "\n")
         add_spacy_ann(word, tag)
 
     pos = pos + 1 + len(word)
-
+    print("pos is currently " + str(pos))
 
 def prompt_for_file_or_dir():
     pass
---
+++ t.py
@@ -0,0 +1,4 @@
+import pickle
+
+f = pickle.load(open("output/test-spacy.pkl", 'rb'))
+print(f)
